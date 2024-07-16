05f5f3e5ab883815373f69a1f4082f3f33f7216a
molybdenum0330 <molybdenum0330@gmail.com>
2024-07-17 01:34:34 +0900

    gh-pages: create

--- src/storage/theme.ts
+++
@@ -1,8 +0,0 @@
-export const save = (darkMode: boolean) => {
-    localStorage.setItem(`darkMode`, darkMode.toString());
-}
-
-export const restore = (): boolean => {
-    const darkMode = localStorage.getItem(`darkMode`);
-    return darkMode ? darkMode === 'true' : false;
-}
